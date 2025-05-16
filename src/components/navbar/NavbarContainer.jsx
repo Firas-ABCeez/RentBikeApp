48fb82dc7342dc4f07516991dc9ff772e7efb21e
Firas <firas.d@abceez.digital>
2025-05-16 22:04:35 +0100

    feat(navbar): added 2nd row for timeline controllers

--- src/components/navbar/NavbarContainer.jsx
+++ src/components/navbar/NavbarContainer.jsx
@@ -6,15 +6,19 @@ import { Input } from "@/components/ui/input"
 // ICONS
 import { IoLogInOutline } from "react-icons/io5";
 import { CiSearch } from "react-icons/ci";
-import electricBike_white from '@/assets/icons/electricBike_white.svg'
+import electricBike_white from '@/assets/icons/electricBike_white.svg';
+import realTime_white from '@/assets/icons/realTime_white.svg';
+import tf30min_black from '@/assets/icons/tf30min_black.svg';
+import tf60min_black from '@/assets/icons/tf60min_black.svg';
+
 
 
 export default function NavbarContainer() {
     return (
-        <div className="bg-white w-full h-[150px]">
+        <div className="w-full h-fit space-y-2 py-2">
 
             {/* 1ST ROW: Controllers + Auth BTN */}
-            <div className="flex flex-row justify-between p-5">
+            <div className="flex flex-row justify-between px-5">
 
                 {/* LEFT SECTION */}
                 <div className="min-w-[415px] flex justify-between items-center">
@@ -58,10 +62,11 @@ export default function NavbarContainer() {
                             justify-between 
                             cursor-pointer 
                             !px-5 
-                            py-5 bg-[#10ac84]
-                            hover:bg-[#1dd1a1] 
+                            py-5 
+                            bg-[#019b94]
+                            hover:bg-[#0fb9b1] 
                             border-[1px] 
-                            border-[#10ac84]
+                            border-[#019b94]
                             hover:text-white
                             text-white 
                             rounded-full 
@@ -118,6 +123,147 @@ export default function NavbarContainer() {
                     </Button>
                 </div>
 
+            </div>
+
+            {/* 2ST ROW: Timeline Controllers */}
+            <div className="flex flex-row justify-between px-5">
+
+
+                {/* CONTAINER SECTION */}
+                <div className="
+                            min-w-[415px] 
+                            flex 
+                            justify-evenly 
+                            items-center 
+                            bg-white
+                            border-[1px]
+                            border-gray-400 
+                            font-semibold 
+                            !text-sm
+                            rounded-full
+                            m-0
+                            shadow-sm
+                            ">
+
+                    {/* REAL TIME BTN */}
+                    <Button
+                        variant="outline"
+                        className="
+                            w-[138.333333333px]
+                            flex
+                            flex-row 
+                            items-center 
+                            justify-between 
+                            cursor-pointer 
+                            !px-5 
+                            py-5 
+                            bg-[#F53B57]
+                            hover:bg-[#ff5e57] 
+                            border-[1px] 
+                            border-[#F53B57]
+                            hover:text-white
+                            text-white 
+                            rounded-full 
+                            font-extrabold 
+                            !text-sm 
+                            float-end
+                            shadow-md
+                            hover:shadow-lg">
+
+                        {/* ICON */}
+                        <span
+                            style={{
+                                backgroundImage: `url(${realTime_white})`,
+                                backgroundRepeat: 'no-repeat',
+                                backgroundSize: 'contain',
+                                display: 'inline-block',
+                                width: '24px',
+                                height: '24px',
+                            }}
+                        ></span>
+
+                        {/* BTN TEXT */}
+                        <span className="mt-[1px]">Real time</span>
+                    </Button>
+
+                     {/* 30 MIN BTN */}
+                    <Button
+                        variant="outline"
+                        className="
+                            w-[138.333333333px]
+                            flex
+                            flex-row 
+                            items-center 
+                            justify-between 
+                            cursor-pointer 
+                            !px-5 
+                            py-5 
+                            bg-white
+                            hover:bg-[#ffbf8b]  
+                            hover:border-[#ffbf8b]
+                            border-[1px] 
+                            border-white
+                            text-gray-700 
+                            rounded-full 
+                            font-semibold 
+                            !text-sm 
+                           ">
+
+                        {/* ICON */}
+                        <span
+                            style={{
+                                backgroundImage: `url(${tf30min_black})`,
+                                backgroundRepeat: 'no-repeat',
+                                backgroundSize: 'contain',
+                                display: 'inline-block',
+                                width: '24px',
+                                height: '24px',
+                            }}
+                        ></span>
+
+                        {/* BTN TEXT */}
+                        <span className="mt-[1px]">In 30 Min</span>
+                    </Button>
+
+                    {/* 60 MIN BTN */}
+                    <Button
+                        variant="outline"
+                        className="
+                            w-[138.333333333px]
+                            flex
+                            flex-row 
+                            items-center 
+                            justify-between 
+                            cursor-pointer 
+                            !px-5 
+                            py-5 
+                            bg-white
+                            hover:bg-[#8fffc7]  
+                            hover:border-[#8fffc7]
+                            border-[1px] 
+                            border-white
+                            text-gray-700 
+                            rounded-full 
+                            font-semibold 
+                            !text-sm 
+                           ">
+
+                        {/* ICON */}
+                        <span
+                            style={{
+                                backgroundImage: `url(${tf60min_black})`,
+                                backgroundRepeat: 'no-repeat',
+                                backgroundSize: 'contain',
+                                display: 'inline-block',
+                                width: '24px',
+                                height: '24px',
+                            }}
+                        ></span>
+
+                        {/* BTN TEXT */}
+                        <span className="mt-[1px]">In 60 Min</span>
+                    </Button>
+                </div>
             </div>
         </div>
     )
