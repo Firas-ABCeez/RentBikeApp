ee3e9f91d71c68aa68c0c79e5092ec563525fc98
Firas <firas.d@abceez.digital>
2025-05-17 15:35:37 +0100

    feat(navbar):Implement authentication dialog/popup

--- src/components/navbar/UpperRow.jsx
+++ src/components/navbar/UpperRow.jsx
@@ -3,6 +3,14 @@ import { useState, useEffect, useRef } from "react";
 // UI COMPONENTS
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
 
 
 // ICONS
@@ -14,7 +22,9 @@ import parking_white from '@/assets/icons/parking_white.svg';
 import electricBike_color from '@/assets/icons/electricBike_color.svg';
 import normalBike_color from '@/assets/icons/normalBike_color.svg';
 import parking_color from '@/assets/icons/parking_color.svg';
-
+import logo from '@/assets/icons/logo.png';
+import { BiLogInCircle } from "react-icons/bi";
+import googleIcon from '@/assets/icons/googleIcon.svg';
 
 
 export default function UpperRow() {
@@ -111,7 +121,7 @@ export default function UpperRow() {
 
 
                 {/* SELECT MENU */}
-                <div ref={menuRef}> 
+                <div ref={menuRef}>
                     <Button
                         variant="outline"
                         className="
@@ -220,32 +230,219 @@ export default function UpperRow() {
             </div>
 
             {/* RIGHT SECTION: Auth Btn */}
-            <div className="w-full">
-                <Button
-                    variant="outline"
-                    className="
-                        flex
-                        flex-row
-                        items-center
-                        justify-between
-                        cursor-pointer
-                        !px-5
-                        py-5 bg-[#2e86de]
-                        hover:bg-[#54a0ff]
-                        border-[1px]
-                        border-[#54a0ff]
-                        hover:text-white
-                        text-white
-                        rounded-full
-                        font-extrabold
-                        !text-sm
-                        float-end
-                        shadow-md
-                        hover:shadow-lg
-                        ">
-                    <IoLogInOutline className="size-[25px]" />
-                    <span className="mt-[1px]">Join Now</span>
-                </Button>
+            <div className="w-full flex items-center justify-end">
+
+                {/* AUTH DIALOG */}
+                <Dialog>
+                    <DialogTrigger>
+                        <Button
+                            variant="outline"
+                            className="
+                              justify-between
+                              cursor-pointer
+                              !px-5
+                              py-5 bg-[#2e86de]
+                              hover:bg-[#54a0ff]
+                              border-[1px]
+                              border-[#54a0ff]
+                              hover:text-white
+                              text-white
+                              rounded-full
+                              font-extrabold
+                              !text-sm
+                              float-end
+                              shadow-md
+                              hover:shadow-lg
+                             ">
+                            <IoLogInOutline className="size-[25px]" />
+                            <span className="mt-[1px]">Join Now</span>
+                        </Button>
+                    </DialogTrigger>
+                    <DialogContent>
+                        <DialogHeader>
+                            <DialogTitle className="text-center">
+                                <span style={{
+                                    backgroundImage: `url(${logo})`,
+                                    backgroundSize: 'contain',
+                                    backgroundRepeat: 'no-repeat',
+                                    display: 'inline-block',
+                                    width: '300px',
+                                    height: '200px',
+                                }}></span>
+                            </DialogTitle>
+                            <DialogDescription className="-mt-[45px]">
+
+                                {/* FORM SECTION (EMAIL + PASSWORD + LOGIN BTN + FORGOT PASSWORD LINK) */}
+                                <div className="max-w-[400px] mx-auto space-y-3">
+
+                                    {/* EMAIL INPUT */}
+                                    <div>
+                                        <Input
+                                            type="email"
+                                            placeholder="Enter your email address..."
+                                            className="
+                                          bg-gray-100
+                                           border-[1px]
+                                         border-gray-200
+                                           p-5
+                                           text-black
+                                           font-semibold
+                                           !text-sm
+                                           rounded-full
+                                           m-0
+                                           hover:shadow-sm
+                                          "/>
+                                    </div>
+
+                                    <div className="flex flex-row space-x-3">
+
+                                        {/* PASSWORD INPUT */}
+                                        <div>
+                                            <Input
+                                                type="password"
+                                                placeholder="Enter your password..."
+                                                className="
+                                                bg-gray-100
+                                                border-[1px]
+                                                border-gray-200
+                                                p-5
+                                                w-[295px]
+                                                font-semibold
+                                                !text-sm
+                                                text-black
+                                                rounded-full
+                                                hover:shadow-sm
+                                                m-0
+                                            "/>
+                                        </div>
+
+                                        {/* LOGIN BTN */}
+                                        <div>
+                                            <Button
+                                            type="submit"
+                                                placeholder="Enter your email address..."
+                                                className="
+                                                bg-[#2E86DE]
+                                                hover:bg-[#2ea9de]
+                                                p-5
+                                                text-white
+                                                font-semibold
+                                                border-[1px]
+                                                border-[#2ea9de]
+                                                rounded-full
+                                                !text-sm
+                                                shadow-sm
+                                                m-0
+                                                hover:shadow-md
+                                                cursor-pointer
+                                                flex
+                                                flex-row 
+                                                items-center
+                                                justify-evenly
+                                          ">
+                                                <BiLogInCircle className="size-[25px]" />
+                                                <span className="font-bold">
+                                                    Login
+                                                </span>
+                                            </Button>
+                                        </div>
+                                    </div>
+
+                                    {/* FORGOT PASSWORD LINK */}
+                                    <div className="mt-5">
+                                        <a href="#"
+                                            className="
+                                            hover:text-blue-500 
+                                           text-blue-600 
+                                            font-normal
+                                            ml-2
+                                            text-sm">
+                                            Forgot your password?
+                                        </a>
+                                    </div>
+
+                                    {/* BREAK */}
+                                    <p className="text-center font-medium text-gray-400 text-[13px]">—OR—</p>
+
+                                    {/* CREATE ACCOUNT + 3RD PARTY AUTH BTN */}
+                                    <div className="flex flex-row items-center justify-between space-x-2 mt-4">
+
+                                        {/* CREATE ACCOUNT */}
+                                        <div>
+                                            <Button
+                                                type="text"
+                                                placeholder="Enter your email address..."
+                                                className="
+                                                bg-[#22D074]
+                                                hover:bg-[#22d059]
+                                                p-5
+                                                text-white
+                                                border-[1px]
+                                                border-[#22d059]
+                                                rounded-full
+                                                !text-sm
+                                                shadow-sm
+                                                m-0
+                                                hover:shadow-md
+                                                cursor-pointer
+                                                w-[180px]
+                                                flex
+                                                flex-row 
+                                                items-center
+                                                justify-evenly
+                                                font-bold
+                                          ">
+                                                Start new account
+                                            </Button>
+                                        </div>
+
+                                        {/* 3RD PARTY AUTH BTN */}
+                                        <div>
+                                            <Button
+                                                type="text"
+                                                placeholder="Enter your email address..."
+                                                className="
+                                                bg-gray-5
+                                                hover:bg-gray-100
+                                                p-5
+                                                text-gray-800
+                                                border-[1px]
+                                                border-gray-200
+                                                rounded-full
+                                                !text-sm
+                                                shadow-sm
+                                                m-0
+                                                hover:shadow-md
+                                                cursor-pointer
+                                                w-[210px]
+                                                flex
+                                                flex-row 
+                                                items-center
+                                                justify-evenly
+                                          ">
+                                                <span style={{
+                                                    backgroundImage: `url(${googleIcon})`,
+                                                    backgroundRepeat: 'no-repeat',
+                                                    backgroundSize: 'cover',
+                                                    display: 'inline-block',
+                                                    padding: '20px',
+                                                }}
+                                                className="mr-[-5px] ml-[-13px]"
+                                                ></span>
+                                                <span className="font-bold">
+                                                    Continue with Google
+                                                </span>
+                                            </Button>
+                                        </div>
+                                    </div>
+
+                                </div>
+
+                            </DialogDescription>
+                        </DialogHeader>
+                    </DialogContent>
+                </Dialog>
+
             </div>
 
         </div>
